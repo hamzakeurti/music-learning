bfbd5f6a8f2c5e1715cf8869ae28ea9dbd8abd34
Hamza Keurti <hamza.keurti@gmail.com>
2019-06-08 00:22:19 +0800

    download

--- scripts/download_data.py
+++ scripts/download_data.py
@@ -1,14 +1,7 @@
-import sys,os,errno,signal,copy
-from contextlib import contextmanager
-
-import numpy as np
 import musicnet
 
-import torch
-
 root = 'home/hamza/data/musicnet'
-
-
 window = 16384
+
 #starting download
 train_set = musicnet.MusicNet(root=root, train=True, download=True, window=window, mmap=False)#, pitch_shift=5, jitter=.1)
