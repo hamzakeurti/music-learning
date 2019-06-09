8beb6ce1b481318bf123b1d76f25dc40506c941f
ValentinYiu <valentinY@protonmail.com>
2019-06-09 10:31:16 +0800

    window

--- musicdownload.py
+++ musicdownload.py
@@ -15,5 +15,7 @@ try:
 except OSError as e:
     if e.errno != errno.EEXIST:
         raise
+
+window = 16384
 #starting download
 train_set = musicnet.MusicNet(root=root, train=True, download=True, window=window, mmap=False)#, pitch_shift=5, jitter=.1)
