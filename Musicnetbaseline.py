a444f132a70f6342e38a7fa7aa2c8ceafbf41c80
ValentinYiu <valentinY@protonmail.com>
2019-06-09 16:03:23 +0800

    baseline

---
+++ Musicnetbaseline.py
@@ -0,0 +1,114 @@
+import sys,os,errno,signal,copy
+from contextlib import contextmanager
+
+import numpy as np
+import musicnet
+
+import torch
+from torch.autograd import Variable
+from torch.nn.functional import conv1d, mse_loss
+
+from time import time
+
+import matplotlib.pyplot as plt
+from IPython.display import Audio
+
+from sklearn.metrics import average_precision_score
+import argparse
+from model import Model
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--mmap',default=0,type=int)
+parser.add_argument('--batch_size',default=100,type=int)
+args = parser.parse_args()
+
+root = './musicnet'
+checkpoint_path = './checkpoints'
+checkpoint = 'musicnet_demo.pt'
+
+try:
+    os.makedirs(checkpoint_path)
+except OSError as e:
+    if e.errno != errno.EEXIST:
+        raise
+
+os.environ['CUDA_DEVICE_ORDER']='PCI_BUS_ID'   # see issue #152
+os.environ['CUDA_VISIBLE_DEVICES']='0'
+
+m = 128
+k = 500
+d = 4096
+window = 16384
+stride = 512
+batch_size = args.batch_size
+
+regions = int(1 + (window - d)/stride)
+
+mmap=True if args.mmap==1 else False
+train_set = musicnet.MusicNet(root=root, train=True, window=window, mmap=mmap)#, pitch_shift=5, jitter=.1)
+test_set = musicnet.MusicNet(root=root, train=False, window=window, epoch_size=5000,mmap = mmap)
+train_loader = torch.utils.data.DataLoader(dataset=train_set,batch_size=batch_size)
+test_loader = torch.utils.data.DataLoader(dataset=test_set,batch_size=batch_size)
+
+
+def L(y_hat, y):
+    # adjust for per-frame loss
+    return mse_loss(y_hat, y)*128/2.
+
+@contextmanager
+def averages(model):
+    orig_parms = copy.deepcopy(list(parm.data for parm in model.parameters()))
+    for parm, pavg in zip(model.parameters(), model.averages):
+        parm.data.copy_(pavg)
+    yield
+    for parm, orig in zip(model.parameters(), orig_parms):
+        parm.data.copy_(orig)
+
+model = Model()
+print(model)
+loss_history = []
+avgp_history = []
+
+try:
+    model.load_state_dict(torch.load(os.path.join(checkpoint_path,checkpoint)))
+except IOError as e:
+    if e.errno != errno.ENOENT:
+        raise
+optimizer = torch.optim.SGD(model.parameters(), lr=0.000001, momentum=.95)
+
+try:
+    with train_set, test_set:
+        print('square loss\tavg prec\ttime\t\tutime')
+        for epoch in range(50):
+            t = time()
+            for i, (x, y) in enumerate(train_loader):
+                x, y = Variable(x.cuda(), requires_grad=False), Variable(y.cuda(), requires_grad=False)
+                loss = L(model(x),y)
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+                model.average_iterates()
+                if i%1000==0:
+                    print(i)
+                    print(loss)
+            t1 = time()
+            avgp, loss = 0., 0.
+            yground = torch.FloatTensor(batch_size*len(test_loader), m)
+            yhat = torch.FloatTensor(batch_size*len(test_loader), m)
+            with averages(model):
+                for i, (x, y) in enumerate(test_loader):
+                    x, y = Variable(x.cuda(), requires_grad=False), Variable(y.cuda(), requires_grad=False)
+                    yhatvar = model(x)
+                    loss += L(yhatvar,y).data.item()
+                    yground[i*batch_size:(i+1)*batch_size,:] = y.data
+                    yhat[i*batch_size:(i+1)*batch_size,:] = yhatvar.data
+            avgp = average_precision_score(yground.numpy().flatten(),yhat.numpy().flatten())
+            loss_history.append(loss/len(test_loader))
+            avgp_history.append(avgp)
+            torch.save(model.state_dict(), os.path.join(checkpoint_path,checkpoint))
+            print('{:2f}\t{:2f}\t{:2f}\t{:2f}'.format(loss_history[-1],avgp_history[-1],time()-t, time()-t1))
+
+except KeyboardInterrupt:
+    print('Graceful Exit')
+else:
+    print('Finished')
--- model.py
+++ model.py
@@ -1,12 +1,56 @@
+import torch
 import torch.nn as nn
-import torch.functional as F
-class DCNN(nn.Module):
-    def __init__(self):
-        super(DCNN, self).__init__()
-    self.conv2048 = nn.conv1d(1,64,2048,strides=512,padding=2048//2)
-    self.conv4096 = nn.conv1d(1,64,4096,strides=512,padding=4096//2)
-    self.conv8192 = nn.conv1d(1,64,8192,strides=512,padding=8192//2)
-    self.conv16384 = nn.conv1d(1,64,16384,strides=512,padding=16384//2)
-    def forward(self,x):
-        # Size len(input)//512
-        c2048 = self.conv2048(x)
+from torch.autograd import Variable
+from torch.nn.functional import conv1d, mse_loss
+import musicnet
+import numpy as np
+import copy
+
+def create_filters(d,k,low=50,high=6000):
+    x = np.linspace(0, 2*np.pi, d, endpoint=False)
+    wsin = np.empty((k,1,d), dtype=np.float32)
+    wcos = np.empty((k,1,d), dtype=np.float32)
+    start_freq = low
+    end_freq = high
+    num_cycles = start_freq*d/44000.
+    scaling_ind = np.log(end_freq/start_freq)/k
+    window_mask = 1.0-1.0*np.cos(x)
+    for ind in range(k):
+        wsin[ind,0,:] = window_mask*np.sin(np.exp(ind*scaling_ind)*num_cycles*x)
+        wcos[ind,0,:] = window_mask*np.cos(np.exp(ind*scaling_ind)*num_cycles*x)
+    
+    return wsin,wcos
+
+
+class Model(torch.nn.Module):
+    def __init__(self, avg=.9998,stride=512,regions=25,d=4096,k=500):
+        super(Model, self).__init__()
+        
+        wsin,wcos = create_filters(d,k)
+        with torch.cuda.device(0):
+            self.wsin_var = Variable(torch.from_numpy(wsin).cuda(), requires_grad=False)
+            self.wcos_var = Variable(torch.from_numpy(wcos).cuda(), requires_grad=False)
+        
+        self.linear = torch.nn.Linear(regions*k, m, bias=False).cuda()
+        torch.nn.init.constant(self.linear.weight, 0)
+
+        self.stride=stride
+        self.regions=regions
+        self.k=k
+
+
+        self.avg = avg
+        self.averages = copy.deepcopy(list(parm.data for parm in self.parameters()))
+        for (name,parm),pavg in zip(self.named_parameters(),self.averages):
+            name = name.split('.')[0]
+            self.register_buffer(name + '_avg', pavg)
+    
+    def forward(self, x):
+        zx = conv1d(x[:,None,:], self.wsin_var, stride=self.stride).pow(2) \
+           + conv1d(x[:,None,:], self.wcos_var, stride=self.stride).pow(2)
+        return self.linear(torch.log(zx + musicnet.epsilon).view(x.data.size()[0],self.regions*self.k))
+    
+    def average_iterates(self):
+        for parm, pavg in zip(self.parameters(), self.averages):
+            pavg.mul_(self.avg).add_(1.-self.avg, parm.data)
+
